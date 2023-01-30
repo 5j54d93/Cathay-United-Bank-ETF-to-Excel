8307d807dab240e8998310f1f9e8538782715ddb
Ricky Chuang <songsongyyohya@gmail.com>
2023-01-30 13:22:39 +0800

    improing code by extracting func

--- main.py
+++ main.py
@@ -1,49 +1,73 @@
-from selenium.webdriver.common.by import By
+import os.path
+import openpyxl
 from openpyxl.styles import Font
 from selenium import webdriver
-import openpyxl
-import os.path
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-url = "https://www.cathaybk.com.tw/cathaybk/personal/investment/etf/search/?hotissue=05"
-safari = webdriver.Safari()
-safari.get(url)
-time.sleep(9)
+def setUpDriver():
+    safari = webdriver.Safari()
+    safari.maximize_window()
+    safari.implicitly_wait(15)
+    return safari
 
-table = safari.find_element(By.ID, "hasResult")
-thead = table.find_element(By.CLASS_NAME, "cubinvest-l-table__tr")
-tbody = table.find_element(By.ID, "resultContainer")
-columnTitles = thead.find_elements(By.XPATH, '//div[@class="cubinvest-l-table__th"][not(@style="display: none;")]')
-columnTitles.pop(0)
-columnTitles.pop(len(columnTitles)-1)
-rows = tbody.find_elements(By.CLASS_NAME, "cubinvest-l-table__tr")
+def waitForWebsiteLoadingComplete(driver):
+    table = driver.find_element(By.ID, "hasResult")
+    thead = table.find_element(By.CLASS_NAME, "cubinvest-l-table__tr")
+    tbody = table.find_element(By.ID, "resultContainer")
+    rows = tbody.find_elements(By.CLASS_NAME, "cubinvest-l-table__tr")
+    lastRow = rows[len(rows) - 1]
+    cells = lastRow.find_elements(By.XPATH, './/div[contains(@class, "cubinvest-l-table__td")][not(@style="display: none;")][not(@style="display:none;")]')
+    nameCell = cells[2]
+    etfId = nameCell.find_element(By.XPATH, './/div[@class="cubinvest-l-table__item cubinvest-l-table__item--fundId"]')
+    WebDriverWait(driver, 15).until_not(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="' + etfId.text + '-DjMktPrice"]'), "-"))
+    return (table, thead, tbody)
 
-if os.path.isfile('Cathay-United-Bank-ETF.xlsx'):
+def initialExcelFormat(workbook):
+    sheet = workbook.worksheets[0]
+    sheet.row_dimensions[1].height = 20
+    sheet.column_dimensions['B'].width = 35
+    sheet.column_dimensions['C'].width = 10
+    return sheet
+
+def writeTableHeadTitle(sheet, thead):
+    columnTitles = thead.find_elements(By.XPATH, '//div[@class="cubinvest-l-table__th"][not(@style="display: none;")]')
+    columnTitles.pop(0)
+    columnTitles.pop(len(columnTitles)-1)
+    for idx, title in enumerate(columnTitles):
+        sheet.cell(row = 1, column = idx + 1).value = title.text.strip()
+        sheet.cell(row = 1, column = idx + 1).font = Font(bold=True)
+
+def writeTableData(sheet, tbody):
+    rows = tbody.find_elements(By.CLASS_NAME, "cubinvest-l-table__tr")
+    for RowIdx, row in enumerate(rows):
+        cells = row.find_elements(By.XPATH, './/div[contains(@class, "cubinvest-l-table__td")][not(@style="display: none;")][not(@style="display:none;")]')
+        cells.pop(0)
+        cells.pop(len(cells)-1)
+        for cellIdx, cell in enumerate(cells):
+            if (cell.text.__contains__("基金")):
+                sheet.cell(row=RowIdx+2, column=cellIdx+1).value = cell.text[:cell.text.index("基金")+2] + "\n" + cell.text[cell.text.index("基金")+2:]
+            elif (cell.get_attribute("data-th") == "DjMktPrice"):
+                elements = cell.find_elements(By.CLASS_NAME, 'cubinvest-l-table__item')
+                sheet.cell(row=RowIdx+2, column=cellIdx+1).value = elements[0].text + "\n" + elements[1].text
+            else:
+                sheet.cell(row=RowIdx+2, column=cellIdx+1).value = cell.text
+                sheet.cell(row=RowIdx+2, column=cellIdx+1).font = Font(color='ff0000') if cell.text.startswith("-") else Font(color='000000')
+
+safari = setUpDriver()
+safari.get("https://www.cathaybk.com.tw/cathaybk/personal/investment/etf/search/?hotissue=05")
+
+if os.path.isfile('Cathay-United-Bank-ETF.xlsx'): # open or new a xlsx file
     workbook = openpyxl.load_workbook('Cathay-United-Bank-ETF.xlsx')
 else:
     workbook = openpyxl.Workbook()
 
-sheet = workbook.worksheets[0]
-sheet.row_dimensions[1].height = 20
-sheet.column_dimensions['B'].width = 35
-sheet.column_dimensions['C'].width = 10
-
-for idx, title in enumerate(columnTitles):
-    sheet.cell(row=1, column=idx+1).value = title.text.strip()
-    sheet.cell(row=1, column=idx+1).font = Font(bold=True)
-
-for RowIdx, row in enumerate(rows):
-    cells = row.find_elements(By.XPATH, './/div[contains(@class, "cubinvest-l-table__td")][not(@style="display: none;")][not(@style="display:none;")]')
-    cells.pop(0)
-    cells.pop(len(cells)-1)
-    for cellIdx, cell in enumerate(cells):
-        if (cell.text.__contains__("基金")):
-            sheet.cell(row=RowIdx+2, column=cellIdx+1).value = cell.text[:cell.text.index("基金")+2] + "\n" + cell.text[cell.text.index("基金")+2:]
-        elif (cell.get_attribute("data-th") == "DjMktPrice"):
-            elements = cell.find_elements(By.CLASS_NAME, 'cubinvest-l-table__item')
-            sheet.cell(row=RowIdx+2, column=cellIdx+1).value = elements[0].text + "\n" + elements[1].text
-        else:
-            sheet.cell(row=RowIdx+2, column=cellIdx+1).value = cell.text
-            sheet.cell(row=RowIdx+2, column=cellIdx+1).font = Font(color='ff0000') if cell.text.startswith("-") else Font(color='000000')
+sheet = initialExcelFormat(workbook)
+(table, thead, tbody) = waitForWebsiteLoadingComplete(safari)
+writeTableHeadTitle(sheet, thead)
+writeTableData(sheet, tbody)
 
 workbook.save('Cathay-United-Bank-ETF.xlsx')
+workbook.close()
+safari.quit()
